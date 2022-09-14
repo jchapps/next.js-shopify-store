42fc80b2598a5ebdee2a2f72f912fd3ae7e8e07b
Josh Chappelow <jdchappelow@gmail.com>
2022-09-14 22:24:33 +0900

    deploy

--- utils/index.js
+++ utils/index.js
@@ -1,12 +1,18 @@
+const domain = process.env.NEXT_PUBLIC_API_URL;
+const storefrontAccessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
+
 export async function storefront(query, variables = {}) {
-  const response = await fetch(process.env.NEXT_PUBLIC_API_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Shopify-Storefront-Access-Token": process.env.NEXT_PUBLIC_ACCESS_TOKEN,
-    },
-    body: JSON.stringify({ query, variables }),
-  });
+  const response = await fetch(
+    `https://${domain}.myshopify.com/api/2021-07/graphql.json`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Shopify-Storefront-Access-Token": storefrontAccessToken,
+      },
+      body: JSON.stringify({ query, variables }),
+    }
+  );
   return response.json();
 }
 
