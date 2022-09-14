7fb9ab53af7fa3e660898b8fefd809b333b6519a
Josh Chappelow <jdchappelow@gmail.com>
2022-09-14 22:31:17 +0900

    deploy

--- utils/index.js
+++ utils/index.js
@@ -1,12 +1,16 @@
 export async function storefront(query, variables = {}) {
-  const response = await fetch(process.env.NEXT_PUBLIC_API_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Shopify-Storefront-Access-Token": process.env.NEXT_PUBLIC_ACCESS_TOKEN,
-    },
-    body: JSON.stringify({ query, variables }),
-  });
+  const response = await fetch(
+    "https://belatedbicycles.myshopify.com/api/2022-07/graphql.json",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Shopify-Storefront-Access-Token":
+          process.env.NEXT_PUBLIC_ACCESS_TOKEN,
+      },
+      body: JSON.stringify({ query, variables }),
+    }
+  );
   return response.json();
 }
 
