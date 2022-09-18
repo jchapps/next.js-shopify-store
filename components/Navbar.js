5f139c11cd8a2de70de68fe18cf606e6ef1304a5
Josh Chappelow <jdchappelow@gmail.com>
2022-09-18 23:22:36 +0900

    navbar styling

--- components/Navbar.js
+++ components/Navbar.js
@@ -46,7 +46,7 @@ function Navbar() {
               <button
                 onClick={() => setIsOpen(!isOpen)}
                 type="button"
-                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                className="bg-green-200 inline-flex items-center justify-center p-2 rounded-md text-black-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                 aria-controls="mobile-menu"
                 aria-expanded="false"
               >
@@ -103,21 +103,21 @@ function Navbar() {
               <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
                 <a
                   href="/"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  className="text-black hover:bg-green-200 hover:text-black block px-3 py-2 rounded-md text-base font-medium"
                 >
                   Products
                 </a>
 
                 <a
                   href="/contact"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  className="text-black hover:bg-green-2000 hover:text-black block px-3 py-2 rounded-md text-base font-medium"
                 >
                   Contact
                 </a>
 
                 <a
                   href="/login"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  className="text-black hover:bg-green-200 hover:text-black block px-3 py-2 rounded-md text-base font-medium"
                 >
                   Login
                 </a>
