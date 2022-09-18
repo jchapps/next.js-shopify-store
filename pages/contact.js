7de4b1a8bf2bca48bc4ca582ffe3b2f74bd25c3d
Josh Chappelow <jdchappelow@gmail.com>
2022-09-19 00:34:21 +0900

    contact us styling

--- pages/contact.js
+++ pages/contact.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div>
       <section className="text-gray-700 body-font relative">
-        <div className="container px-5 py-16 mx-auto">
+        <div className="container px-5 mb-32 mt-10 mx-auto">
           <div className="flex flex-col text-center w-full mb-12">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
               Contact Us
